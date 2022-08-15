3d44a28882bff6329843d0bac4839d41735cee18
1sanyo <sanyo.adonai@student.moringaschool.com>
2022-08-15 06:02:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  "Hi, my name is [Lynton] and I am learning to program in [javascript]."
+  function sayHelloTo(firstName = "Lynton") {
+    console.log(`Hello, ${Lynton}!`);
+  }
